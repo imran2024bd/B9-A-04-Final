7d781144ebb7e194f000e990515380043e0dc7f3
imran2024bd <imranitbd34@gmail.com>
2024-02-08 00:45:44 +0600

    Help The Zoo Manager problem solved

--- assignment.js
+++ assignment.js
@@ -1,5 +1,22 @@
 // Problem-01 : Help The Zoo Manager
 
 function calculateMoney(ticketSale) {
-    
+    const perTicketprice = 120;
+    const securityExpense = 500;
+    const totalStuffnumber = 8;
+    const perStuffexp = 50;
+    if (ticketSale < 0) {
+        return 'Invalid Number , put positive intiger number';
+    }
+    else {
+        const savingsMoney = (ticketSale * perTicketprice) -(securityExpense + (totalStuffnumber * perStuffexp));
+        return savingsMoney;
+    }
+
+
 }
+
+console.log(calculateMoney(10));
+console.log(calculateMoney(1055));
+console.log(calculateMoney(93));
+console.log(calculateMoney(-130));
